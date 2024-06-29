7ed8967656e68cbffb04134e0d48101978b5cb37
lzrs <lzrs@modular.cloud>
2024-06-29 15:17:40 -0700

    Add baselime

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    instrumentationHook: true,
     serverActions: {
       allowedOrigins: ["*.lzrs.workers.dev", "*.modular.cloud"],
     },
