8c40f947b72692db8f63bf28ad17b22f5dd37c1c
lzrs <lzrs@modular.cloud>
2024-07-01 09:56:10 -0700

    Remove baselime integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    instrumentationHook: true,
     serverActions: {
       allowedOrigins: ["*.lzrs.workers.dev", "*.modular.cloud"],
     },
